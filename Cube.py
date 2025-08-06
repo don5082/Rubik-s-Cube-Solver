0d1c1a7e061bba75683e996a358bf272550a0f3d
Dylan Nichols <don5082@rit.edu>
2025-08-05 21:13:05 -0400

    Updated the Print function to work correctly, Added Solver.py, which currently only calls the initialize cube function and the print function. Also made the default values of the cue work correctly

--- Cube.py
+++ Cube.py
@@ -1,5 +1,23 @@
-class Rubiks_cube:
-    def __init__(self, up_face, left_face, front_face, right_face, back_face, down_face):
+
+class R_cube:
+
+    def __init__(self, up_face=None, left_face=None, front_face=None, right_face=None, back_face=None, down_face=None):
+
+        # Defualt Values
+        if up_face is None:
+            up_face = [["W", "W", "W"] for i in range(3)]
+        if left_face is None:
+            left_face = [["O", "O", "O"] for i in range(3)]
+        if front_face is None:
+            front_face = [["G", "G", "G"] for i in range(3)]
+        if right_face is None:
+            right_face = [["R", "R", "R"] for i in range(3)]
+        if back_face is None:
+            back_face = [["B", "B", "B"] for i in range(3)]
+        if down_face is None:
+            down_face = [["Y", "Y", "Y"] for i in range(3)]
+
+        #Custom Values
         self.up_face = up_face
         self.left_face = left_face
         self.front_face = front_face
@@ -7,66 +25,81 @@ class Rubiks_cube:
         self.back_face = back_face
         self.down_face = down_face
 
-    def print_cube(self, Rubiks_cube):
+    def print_cube(self):
 
         # Print Top grid
     # ___________________________
-        print(' ', end='')
-        print(' ', end='')
-        print(' ', end='')
-
-        print(Rubiks_cube.up_face[0][:2])
+        for i in range(18):
+            print(" ", end='')
 
 
-        print(' ', end='')
-        print(' ', end='')
-        print(' ', end='')
-
-        print(Rubiks_cube.up_face[1][:2])
+        print(self.up_face[0][:3])
 
 
-        print(' ', end='')
-        print(' ', end='')
-        print(' ', end='')
+        for i in range(18):
+            print(" ", end='')
 
-        print(Rubiks_cube.up_face[2][:2])
+
+        print(self.up_face[1][:3])
+
+
+        for i in range(18):
+            print(" ", end='')
+
+
+        print(self.up_face[2][:3])
+
+
+        print()
 
 
         # Print Middle Grids
     # _______________________________________________
 
-        print(Rubiks_cube.left_face[0][:2], end='')
-        print(Rubiks_cube.front_face[0][:2], end='')
-        print(Rubiks_cube.right_face[0][:2], end='')
-        print(Rubiks_cube.back_face[0][:2])
+        print(self.left_face[0][:3], end='')
+        print("   ", end='')
+        print(self.front_face[0][:3], end='')
+        print("   ", end='')
+        print(self.right_face[0][:3], end='')
+        print("   ", end='')
+        print(self.back_face[0][:3])
 
-        print(Rubiks_cube.left_face[1][:2], end='')
-        print(Rubiks_cube.front_face[1][:2], end='')
-        print(Rubiks_cube.right_face[1][:2], end='')
-        print(Rubiks_cube.back_face[1][:2])
 
-        print(Rubiks_cube.left_face[2][:2], end='')
-        print(Rubiks_cube.front_face[2][:2], end='')
-        print(Rubiks_cube.right_face[2][:2], end='')
-        print(Rubiks_cube.back_face[1][:2])
+        print(self.left_face[1][:3], end='')
+        print("   ", end='')
+        print(self.front_face[1][:3], end='')
+        print("   ", end='')
+        print(self.right_face[1][:3], end='')
+        print("   ", end='')
+        print(self.back_face[1][:3])
+
+
+        print(self.left_face[2][:3], end='')
+        print("   ", end='')
+        print(self.front_face[2][:3], end='')
+        print("   ", end='')
+        print(self.right_face[2][:3], end='')
+        print("   ", end='')
+        print(self.back_face[1][:3])
+
+        print()
 
         # Print Bottom Grid
     # _______________________________________________
 
-        print(' ', end='')
-        print(' ', end='')
-        print(' ', end='')
+        for i in range(18):
+            print(" ", end='')
 
-        print(Rubiks_cube.down_face[0][:2])
+        print(self.down_face[0][:3])
 
-        print(' ', end='')
-        print(' ', end='')
-        print(' ', end='')
+        for i in range(18):
+            print(" ", end='')
 
-        print(Rubiks_cube.down_face[1][:2])
 
-        print(' ', end='')
-        print(' ', end='')
-        print(' ', end='')
+        print(self.down_face[1][:3])
 
-        print(Rubiks_cube.down_face[2][:2])
+        for i in range(18):
+            print(" ", end='')
+
+
+        print(self.down_face[2][:3])
---
+++ Solver.py
@@ -0,0 +1,6 @@
+import Cube
+
+
+r_cube = Cube.R_cube()
+
+r_cube.print_cube()
